cd5e09c55e40338d7b9cc4ad9d0a9ee3e7c9daa4
Tutul Deb Roy <debroytutul@gmail.com>
2024-10-12 21:01:39 +0530

    WIP Actionbar added

--- app/src/main/java/com/vecs/MainActivity.kt
+++ app/src/main/java/com/vecs/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.vecs
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.vecs.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,18 +20,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setBackgroundDrawable(getDrawable(R.drawable.appbar_drawable))
+
         val navView: BottomNavigationView = binding.navView
         navView.labelVisibilityMode = BottomNavigationView.LABEL_VISIBILITY_LABELED
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_trace, R.id.navigation_planner,R.id.navigation_more
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.actionbar_menu, menu)
+        return true
     }
 }
